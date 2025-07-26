0527e360664a8db6fdaec1dd5c808bbac28e30fe
mubu002 <mustafa_buga1@hotmail.com>
2025-07-26 16:32:41 +0200

    minioppgave2: registrer input med useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 
 
@@ -6,7 +6,22 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function ToDoApplication() {
-    return <h1>Hei Jeg er ToDo-appen din</h1>;
+    const [input, setInput] = useState('');
+    const [todo, setTodo] = useState('');
+
+    function handleClick(){
+        setTodo(input);
+    }
+
+    return <>
+        <h1>Hei Jeg er ToDo-appen din</h1>
+
+        <div>
+            <input type="text" onChange={(e) => setInput(e.target.value)} value={input} />
+            <button onClick={handleClick}>Registrer</button>
+            <p>Du har registrert {todo}</p>
+        </div>
+    </>;
 }
 
 root.render(<ToDoApplication/>);
